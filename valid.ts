c64acfee249675fc6582da886f033230e44f6739
oarkon <oarkon@hotmail.com>
2019-09-28 23:40:57 +0300

    - objectValue we dont need to falsy values

--- valid.ts
+++ valid.ts
@@ -2,7 +2,9 @@ function valid(value: number | boolean | string, pattern?: Object) {
     let isValid = [];
     if (typeof value != "undefined") {
         if (pattern) {
-            Object.keys(pattern).map(restriction => { 
+            Object.keys(pattern).map(restriction => {                
+                /* if inside of object any value is false, its useless, so we ignore that value*/
+                if (typeof pattern[restriction] == "boolean" && !pattern[restriction]) return delete pattern[restriction];
                 /* we need only "true" values */
                 let result = executeFunctionByName(restriction, window, value, pattern[restriction])
                 if(!result) throw new Error(`${restriction} hatasi!`)
@@ -30,33 +32,22 @@ function valid(value: number | boolean | string, pattern?: Object) {
      * @type boolean | number | string
      * @desc pattern e uygunluk kontrollrinin saglanmasi icin tanimlanan deger
  */
-function executeFunctionByName(functionName, context , arguments , objectValue) {   
-    return context[functionName].apply(context,[arguments, objectValue])
+function executeFunctionByName(functionName, context , arguments ) {   
+    return context[functionName].apply(context,[arguments])
 };
 
 /**
  * The following functions are related to value limitations
  * @param value is the main element in our proccess.
- * @param objectValue value must be to equal to this
  */
-function IsNumber(value, objectValue) {
-    if ((value instanceof Number || typeof value === "number")){
-      if(objectValue) return true
+function IsNumber(value) {
+    if (value instanceof Number || typeof value === "number") return true
       else return false;
-    } else {
-      if(!objectValue) return true
-      else return false;
-    }
 }
 
-function IsString(value, objectValue) {
-    if ((value instanceof String || typeof value === "string")) {
-    if(objectValue) return true
-      else return false;
-    } else {
-      if(!objectValue) return true
-      else return false;
-    }
+function IsString(value) {
+    if (value instanceof String || typeof value === "string") return true
+      else return false;    
 }
 
 valid("3", {IsNumber:false , IsString:true})
